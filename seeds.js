e4419d8a002a1356af0c9cda7b29b20f3b86cb8c
Joe Kariuki <jknguru6@gmail.com>
2018-07-09 21:58:11 +0000

    adding styling to comments and show page

--- seeds.js
+++ seeds.js
@@ -7,6 +7,7 @@ var data = [
         name: "Cloud's Rest", 
         image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg",
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+        // add website link to actual campground
     },
     {
         name: "Desert Mesa", 
@@ -52,7 +53,6 @@ function seedDB() {
             });
         });
     });
-    // add a few comments
 }
 
 module.exports = seedDB;
