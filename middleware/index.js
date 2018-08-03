6ad46d7c4361357cd11fea622a7d82d69fa89131
Joe Kariuki <jknguru6@gmail.com>
2018-08-03 15:01:36 +0000

    adding middleware file

---
+++ middleware/index.js
@@ -0,0 +1,56 @@
+var Campground = require("../models/campground");
+var Comment = require("../models/comment");
+
+// ALL MIDDLEWARE
+var middlwareObj = {};
+    
+middlwareObj.checkCampgroundOwnership = function (req, res, next) {
+    if (req.isAuthenticated()) {
+        Campground.findById(req.params.id, function(err, foundCampground) {
+            if(err) {
+                res.redirect("/campgrounds")
+            } else {
+                 // does user own campground?
+                 if(foundCampground.author.id.equals(req.user._id)) {
+                    next();
+                 } else {
+                    res.redirect("back");
+                 }
+            }
+        });
+    } else {
+        res.redirect("back");
+    }
+};
+
+middlwareObj.checkCommentOwnership = function (req, res, next) {
+    if (req.isAuthenticated()) {
+        Comment.findById(req.params.comment_id, function(err, foundComment) {
+            if(err) {
+                res.redirect("back")
+            } else {
+                 // does user own comment?
+                 if(foundComment.author.id.equals(req.user._id)) {
+                    next();
+                 } else {
+                    res.redirect("back");
+                 }
+            }
+        });
+    } else {
+        res.redirect("back");
+    }
+};
+
+// isloggedIn Middleware
+middlwareObj.isLoggedIn = function (req, res, next) {
+    if(req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/login");
+};
+
+
+
+
+module.exports = middlwareObj;
