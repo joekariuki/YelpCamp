0e539240dd5167b7a2a03a2658b51093af02c9a8
Joe Kariuki <jknguru6@gmail.com>
2018-08-03 13:38:35 +0000

    adding delete comment route and button

--- routes/comments.js
+++ routes/comments.js
@@ -71,7 +71,19 @@ router.put("/:comment_id", function (req, res) {
         } else {
             res.redirect("/campgrounds/" + req.params.id);
         }
-    })
+    });
+});
+
+
+// DELETE COMMENT ROUTE
+router.delete("/:comment_id", function(req, res) {
+    Comment.findByIdAndRemove(req.params.comment_id, function(err) {
+        if(err) {
+            res.redirect("back");
+        } else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
 });
 
 // isloggedIn Middleware
