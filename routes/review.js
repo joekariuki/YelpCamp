588d670168b7b31deb07769af69b6d9ae482db90
Joe Kariuki <jknguru6@gmail.com>
2018-12-18 16:43:21 +0000

    add create route for in review file

--- routes/review.js
+++ routes/review.js
@@ -5,7 +5,7 @@ var Review = require("../models/review");
 var middleware = require("../middleware");
 
 
-// New Reviews Route
+// NEW - takes you to new review page
 router.get("/new", middleware.isLoggedIn, middleware.checkReviewExistence, function (req, res) {
     // middleware.checkReviewExistence checks if user already reviewed the campground
     Campground.findById(req.params.id, function(err, campground) {
@@ -17,4 +17,43 @@ router.get("/new", middleware.isLoggedIn, middleware.checkReviewExistence, funct
     });
 });
 
+// CREATE - creates a review
+router.post("/", middleware.isLoggedIn, middleware.checkReviewExistence, function (req, res) {
+   Campground.findById(req.params.id).populate("reviews").exec(function (err, campground) {
+       if (err) {
+           req.flash("error", err.message);
+           return res.redirect("back");
+       }
+       Review.create(req.body.review, function (err, review) {
+           if(err) {
+               req.flash("error", err.message);
+               return res.redirect("back");
+           }
+           review.author.id = req.user.id;
+           review.author.username = req.user.username;
+           review.campground = campground;
+           // save review
+           review.save();
+           campground.reviews.push(review);
+           // calculate the new average review for th campground
+           campground.rating = calculateAverage(campground.reviews);
+           // save campground
+           campground.save();
+           req.flash("success", "Your review has been successfully added.");
+           res.redirect('/campgrounds/' + campground._id);
+       });
+   });
+});
+
+
+function calculateAverage(reviews) {
+    if (reviews.length === 0) {
+        return 0;
+    }
+    var sum = 0;
+    reviews.forEach(function (element) {
+        sum += element.rating;
+    });
+    return sum / reviews.length;
+}
 module.exports = router;
