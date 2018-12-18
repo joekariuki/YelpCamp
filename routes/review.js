ee5de099756c246007df229f0860c29fba10f298
Joe Kariuki <jknguru6@gmail.com>
2018-12-18 15:56:08 +0000

    add review route code to review route file

--- routes/review.js
+++ routes/review.js
@@ -3,3 +3,18 @@ var router = express.Router({mergeParams: true});
 var Campground = require("../models/campground");
 var Review = require("../models/review");
 var middleware = require("../middleware");
+
+
+// New Reviews Route
+router.get("/new", middleware.isLoggedIn, middleware.checkReviewExistence, function (req, res) {
+    // middleware.checkReviewExistence checks if user already reviewed the campground
+    Campground.findById(req.params.id, function(err, campground) {
+        if (err) {
+            req.flash("error", err.message);
+            return res.redirect("back");
+        }
+        res.render("reviews/new", {campground: campground});
+    });
+});
+
+module.exports = router;
